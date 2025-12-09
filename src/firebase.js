a1372de03e29592ab9cce61a034c20f4bf8c7ee3
Prattaykun <prattaystudent@gmail.com>
2025-12-10 03:02:40 +0530

    chore: update firebase dependency to version 10.14.1 and refactor configuration to use environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -14,15 +14,14 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAP3Kdb3joZJYXkDcuhDouC-EdgBPCGzb0",
-  authDomain: "ayush-final-prototype.firebaseapp.com",
-  databaseURL: "https://ayush-final-prototype-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "ayush-final-prototype",
-  storageBucket: "ayush-final-prototype.appspot.com",
-  messagingSenderId: "330161894778",
-  appId: "1:330161894778:web:d6a0ea62c2e13c94cb12c3",
-  measurementId: "G-1LQY3VB0D3",
-  databaseURL: "https://ayush-final-prototype-default-rtdb.asia-southeast1.firebasedatabase.app",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
